7e4552d5b595d3d6b5020ccb0954e55db4480bf8
LukasElias <lukaslundmajland@gmail.com>
2026-01-27 16:48:48 +0000

    Made some simple home-manager configs

--- configuration.nix
+++ configuration.nix
@@ -66,9 +66,6 @@
   users.users.LukasElias = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-    ];
   };
 
   programs.hyprland = {
--- home.nix
+++ home.nix
@@ -15,6 +15,11 @@
   # release notes.
   home.stateVersion = "25.11"; # Please read the comment before changing.
 
+  gtk = {
+    enable = true;
+    colorScheme = "dark";
+  };
+
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
@@ -23,21 +28,21 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
   home.sessionVariables = {
     EDITOR = "nvim";
   };
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user.name = "LukasElias";
+      user.email = "lukaslundmajland@gmail.com";
+    };
+  };
+
+  programs.gh.enable = true;
 
   programs.home-manager.enable = true;
 }
