e5d8f4c7e4c5e6ff8fe38fa872e4ffe613933d4e
Lukas Elias Lund Majland <lukaslundmajland@gmail.com>
2026-01-27 14:25:07 +0000

    yes...

--- configuration.nix
+++ configuration.nix
@@ -2,13 +2,14 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, inputs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./services.nix
+      inputs.home-manager.nixosModules.default
     ];
 
   nixpkgs.config.allowUnfreePredicate = pkg:
@@ -98,6 +99,13 @@
     spotify
     xdg-desktop-portal-hyprland
   ];
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "LukasElias" = import ./home.nix;
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
