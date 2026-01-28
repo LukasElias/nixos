b0c395e19d4c130a4bd7933b23d428e548a70799
LukasElias <lukaslundmajland@gmail.com>
2026-01-28 13:59:22 +0000

    cache

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,12 @@
       inputs.home-manager.nixosModules.default
     ];
 
+  nix.settings = {
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
+
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
       # Add additional package names here
