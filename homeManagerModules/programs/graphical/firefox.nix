abbac380dab026cb69dbf5853947114bdbc468bc
LukasElias <lukaslundmajland@gmail.com>
2026-07-17 16:19:28 +0200

    Firefox extensions and settings.

--- homeManagerModules/programs/graphical/firefox.nix
+++ homeManagerModules/programs/graphical/firefox.nix
@@ -1,6 +1,8 @@
 {
   config,
   lib,
+  inputs,
+  pkgs,
   ...
 }: {
   options.myHomeManager.programs.graphical.firefox = {
@@ -11,6 +13,112 @@
     programs.firefox = {
       enable = true;
       configPath = "${config.xdg.configHome}/mozilla/firefox";
+      languagePacks = [
+        "en-GB"
+        "da"
+      ];
+      profiles."${config.home.username}" = {
+        name = "${config.home.username}";
+        isDefault = true;
+
+        search = {
+          default = "ddg";
+          engines = {
+            google.metaData.alias = "@g";
+
+            nix-packages = {
+              name = "Nix Packages";
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@np"];
+            };
+
+            nixos-options = {
+              name = "Nix Options";
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    {
+                      name = "type";
+                      value = "options";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@no"];
+            };
+
+            home-manager-options = {
+              name = "Home Manager Options";
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    {
+                      name = "type";
+                      value = "options";
+                    }
+                    {
+                      name = "source";
+                      value = "home_manager";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+
+              icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+              definedAliases = ["@ho"];
+            };
+          };
+        };
+
+        settings = {
+          "browser.backup.restore.enabled" = true;
+          "browser.smartwindow.sidebar.openByDefault" = false;
+          "browser.newtabpage.activity-stream.widgets.weather.enabled" = false;
+          "browser.newtabpage.activity-stream.feeds.topsites" = false;
+          "browser.toolbars.bookmarks.visibility" = "never";
+          "sidebar.main.tools" = "history,bookmarks";
+          "sidebar.verticalTabs" = true;
+          "sidebar.revamp" = true;
+          "sidebar.verticalTabs.dragToPinPromo.dismissed" = true;
+        };
+
+        extensions = {
+          force = true;
+          packages = with inputs.firefox-addons.packages."${pkgs.stdenv.hostPlatform.system}"; [
+            ublock-origin
+            keepassxc-browser
+            vimium
+          ];
+        };
+      };
     };
   };
 }
