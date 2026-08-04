a9b66c839e072ef48779e833ae890e44a7728ae8
LukasElias <lukaslundmajland@gmail.com>
2026-08-04 18:06:44 +0200

    Ok blender is apparently broken in nixpkgs-unstable or smth so I had to make this overlay as suggested by random reddit user. Soooo now it works

--- homeManagerModules/programs/graphical/blender.nix
+++ homeManagerModules/programs/graphical/blender.nix
@@ -9,8 +9,23 @@
   };
 
   config = lib.mkIf config.myHomeManager.programs.graphical.blender.enable {
+    nixpkgs.overlays = [
+      (final: prev: {
+        blender-rocm = prev.symlinkJoin {
+          name = "blender-rocm";
+          paths = [prev.pkgsRocm.blender];
+
+          nativeBuildInputs = [prev.makeWrapper];
+
+          postBuild = ''
+            wrapProgram $out/bin/blender --set LD_PRELOAD "${prev.rocmPackages.rocm-comgr}/lib/libamd_comgr.so.3"
+          '';
+        };
+      })
+    ];
+
     home.packages = with pkgs; [
-      pkgsRocm.blender
+      blender-rocm
     ];
   };
 }
