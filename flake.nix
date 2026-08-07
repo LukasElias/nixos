a8075efabad0474945b4e1c11f5dc8c9690359c9
LukasElias <lukaslundmajland@gmail.com>
2026-08-07 16:45:04 +0200

    Added flake.parts and treefmt-nix to my config

--- flake.nix
+++ flake.nix
@@ -24,46 +24,57 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nix-colors.url = "github:misterio77/nix-colors";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages."${system}";
-  in {
-    nixosConfigurations = {
-      LukasLaptop = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/LukasLaptop/configuration.nix
-          inputs.home-manager.nixosModules.default
-        ];
-      };
-      LukasPC = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          ./hosts/LukasPC/configuration.nix
-          inputs.home-manager.nixosModules.default
-        ];
-      };
-    };
-    devShells."${system}".default = pkgs.mkShell {
-      packages = with pkgs; [
-        # lsp
-        nil
-        lua-language-server
-
-        # formatter
-        alejandra
-        stylua
-
-        # quickshell
-        kdePackages.qtdeclarative
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.treefmt-nix.flakeModule
       ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      perSystem = {pkgs, ...}: {
+        treefmt.programs = {
+          alejandra.enable = true;
+          stylua.enable = true;
+          qmlformat.enable = true;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nil
+            lua-language-server
+            kdePackages.qtdeclarative
+          ];
+        };
+      };
+      flake = {lib, ...}: {
+        nixosConfigurations = {
+          LukasLaptop = lib.nixosSystem {
+            specialArgs = {inherit inputs;};
+            modules = [
+              ./hosts/LukasLaptop/configuration.nix
+              inputs.home-manager.nixosModules.default
+            ];
+          };
+          LukasPC = lib.nixosSystem {
+            specialArgs = {inherit inputs;};
+            modules = [
+              ./hosts/LukasPC/configuration.nix
+              inputs.home-manager.nixosModules.default
+            ];
+          };
+        };
+      };
     };
-  };
 }
